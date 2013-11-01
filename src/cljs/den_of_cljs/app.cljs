eb563c2ccd9d1ad566348ead9551f517bc396999
Ian Truslove <ian.truslove@gmail.com>
2013-10-31 18:52:09 -0600

    Got a basic map going

--- src/cljs/den_of_cljs/app.cljs
+++ src/cljs/den_of_cljs/app.cljs
@@ -1,6 +1,18 @@
 (ns den-of-cljs.app
-  (:require [den-of-cljs.views.home :as home]
-            [clojure.browser.repl]))
+    (:require [clojure.browser.repl]))
+
+(def lat-lon-downtown [39.75 -104.99])
+
+(defn initialize-leaflet-map []
+  (let [map (L.Map. "map")
+        osm-url "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        osm (.tileLayer js/L osm-url)
+        center (clj->js lat-lon-downtown)
+        initial-zoom 15]
+    (-> map
+        (.addLayer osm)
+        (.setView center initial-zoom))))
 
 (defn ^:export home_init []
-  (home/init))
+  (initialize-leaflet-map)
+  )
